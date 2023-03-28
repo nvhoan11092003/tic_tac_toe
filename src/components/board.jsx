48d31c016b536c3ee5fcb60c15ca803c3451c215
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-03-28 11:57:48 +0700

    update undo , hightlight

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -4,12 +4,10 @@ import Square from './square'
 const Board = () => {
     const [game , setGame ] = useState([null, null, null, null, null, null, null, null, null])
     const [player, setPlayer] = useState(true)
-    const [time , setTime] = useState(3)
-    const settimefn = ()=>{
-        setTime(time-1)
-    } 
+    // const [time , setTime] = useState(3)
+    const [undoX , setundoX] = useState(1)
+    const [undoO , setundoO] = useState(1)
     const  handlePlay = (position) => {
-      
         const newGame = game.map((item , idex)=>{
             if (idex === position) {
                 return  player ? "X" : "O"
@@ -17,52 +15,82 @@ const Board = () => {
             return item
         })
         setGame(newGame)
-        setPlayer(!player)
-       
-        const myInterval =  setInterval(settimefn,1000)
-        clearInterval(myInterval)
-        setTime(3)
-        
+        setPlayer(!player)     
     } 
     
+    const undo = (position) => {
+        if (game[position] !== "null" ) {
+            const newGame = game.map((item , index)=>{
+                if (player && game[position]=== "X" && undoX !== 0) {
+                    if (index === position) {
+                        setundoX(0)
+                        return  null
+                    }else{
+                        return item
+                    }
+                }
+                else if (!player && game[position]=== "O" && undoO !== 0) {
+                    if (index === position) {
+                        setundoO(0)
+                        return  null
+                    }else{
+                        return item
+                    }
+                }
+                else return item
+
+                
+            })
+            setGame(newGame)
+            setPlayer(player)  
+        }     
+    } 
     const listwin = [
         [0 ,1 ,2 ],[3 ,4 ,5 ],[6 ,7 ,8 ],
         [0 ,3 ,6 ],[1 ,4 ,7 ],[2 ,5 ,8 ],  
         [0 ,4 ,8 ],[2 ,4 ,6 ],
     ]
-
     const checkwin = ()=>{
         for(let i = 0; i < listwin.length; i++) {
             const [p1, p2, p3] = listwin[i]
-            if (game[p1] === game[p2] && game[p2] === game[p3]) {
-                return game[p1]
+            console.log(listwin[i] ,game[p1],game[p2],game[p3] );
+            if (typeof game[p1] !== "null" && typeof game[p2] !== "null" ) {
+                if (game[p1] === game[p2] && game[p2] === game[p3] ) {
+                    return{ whowin : game[p1] , 
+                        listwin : [p1, p2, p3]
+                    } 
+                }
             }
         }
-        return null
+        return { whowin : "" , 
+            listwin : [null ,null ,null]
+        } 
     }
+
+
 
 const regame = ()=>{
     setGame([null, null, null, null, null, null, null, null, null])
 }
 
-const myInterval =  setInterval(settimefn,1000)
-
-
   return (
-    <div className='grid grid-cols-3 gap-2 mx-auto'>
-        <div className="col-span-3 text-3xl text-green-500">{checkwin()?checkwin()+" Winner" :"" } </div>
+    <div className='grid grid-cols-3 gap-2 mx-auto'>  
+        <div className="col-span-3 text-3xl text-green-500">{checkwin().whowin?checkwin().whowin+" Winner" :"" } </div>
         <div className="col-span-3">Đến lượt : {player? "X":"O"}</div>
-        <div className="col-span-3">Người Chơi X : {player? time+"s":""} </div>
-        <div className="col-span-3">Người Chơi O : {player? "": time+"s"}</div>
-      <Square value={game[0]} handlePlay = {()=> handlePlay(0)}></Square>
-      <Square value={game[1]} handlePlay = {()=> handlePlay(1)}></Square>
-      <Square value={game[2]} handlePlay = {()=> handlePlay(2)}></Square>
-      <Square value={game[3]} handlePlay = {()=> handlePlay(3)}></Square>
-      <Square value={game[4]} handlePlay = {()=> handlePlay(4)}></Square>
-      <Square value={game[5]} handlePlay = {()=> handlePlay(5)}></Square>
-      <Square value={game[6]} handlePlay = {()=> handlePlay(6)}></Square>
-      <Square value={game[7]} handlePlay = {()=> handlePlay(7)}></Square>
-      <Square value={game[8]} handlePlay = {()=> handlePlay(8)}></Square>
+        <div className="col-span-3">Người Chơi X : undo {undoO} </div>
+        <div className="col-span-3">Người Chơi O : undo {undoX}</div>
+    {game.map((item , index)=>{
+        if (checkwin().listwin) {
+            for (let i = 0; i < checkwin().listwin.length; i++) {
+                const element = checkwin().listwin[i];
+                if (element === index) {
+                    return <Square value={item} handlePlay = {()=> handlePlay(index)} undo = {()=> undo(index)} highlight={"bg-red-500"}></Square>
+                }
+            }
+        }
+        // console.log(index);
+        return <Square value={item} handlePlay = {()=> handlePlay(index)} undo = {()=> undo(index)} highlight={false}></Square>
+    })}
       <button onClick={regame} className="col-span-3 p-2 border bg-gray-300">Chơi Lại</button>
     </div>
   )
--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,16 +1,23 @@
 
-const Square = ({value, handlePlay}) => {
+const Square = ({value, handlePlay , undo , highlight}) => {
 
  const squareHandlePlay = () =>{
     if (!value) {
         handlePlay()
     }
- }   
-    
+    if (value) {
+        undo()
+    }
+    console.log(highlight);
+ }
+ let className = "w-[60px] h-[60px] bg-[#14bdac] text-3xl text-white" 
+ if (highlight) {
+    className += " bg-red-500" 
+ } 
 
     return <button
-        onClick={squareHandlePlay}
-        className="w-[60px] h-[60px] bg-[#14bdac] text-3xl text-white">
+        onClick={squareHandlePlay} 
+        className={className}>
         {value}
     </button>
 }
